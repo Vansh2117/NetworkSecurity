92b6eb457ea02e3178f11b328ed18499f49ee731
Vansh2117 <vanshsharma5001@gmail.com>
2025-08-07 18:20:16 +0530

    Setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+'''
+This file is an essential part of packaging and distributing Python projects.
+It is used by setuptools (or disutils in older pyhton versions) to define the configuration
+of your projects, such as its metadata, dependencies, and more
+'''
+
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements()->List[str]:
+    '''
+    This Function will return list of requirements
+    '''
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            # Read lines from the file
+            lines=file.readlines()
+            # Process each line
+            for line in lines:
+                requirement=line.strip()
+                # Ignore the empty lines and -e .
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirement.txt not found")
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Vansh Sharma",
+    author_email="vanshsharma5001@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
